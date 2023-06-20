c2849594dd3aa2fe1303ef96771baa8119097fe2
Luis Jesús Díaz Manzo <luis@drconopoima.com>
2023-06-20 21:35:40 +0200

    Generalize insertAtMid as insertAtNth

--- insert_into_middle_queue/main.go
+++ insert_into_middle_queue/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
     "math"
+	"errors"
 )
 
 type node[T any] struct {
@@ -72,19 +73,18 @@ func (self *doublyLinkedList[T]) traverse(yield func(curr *node[T]) *node[T], en
 	}
 }
 
-func (self *doublyLinkedList[T]) yieldNth(yield func(curr *node[T]) *node[T], entryPoint *node[T], jumps int) *node[T] {
-	if jumps > self.length {
-		return nil
+func (self *doublyLinkedList[T]) yieldNth(yield func(curr *node[T]) *node[T], entryPoint *node[T], position int) ( *node[T], error) {
+	if position > self.length {
+		return nil, errors.New("Could not yield element, requested position exceeds list size")
+	} 
+	if position<=0 {
+		return nil, errors.New("Could not yield element, requested position should be larger or equal than 1")
 	}
 	currNode := entryPoint
-	for i := 1; i < jumps; i++ {
-		if currNode == nil {
-			return currNode
-		} else {
-			currNode=yield(currNode)
-		}
+	for i := 1; i < position; i++ {
+		currNode=yield(currNode)
 	}
-	return currNode
+	return currNode, nil
 }
 
 func (self *doublyLinkedList[T]) yieldNext(curr *node[T]) *node[T]{
@@ -95,35 +95,42 @@ func (self *doublyLinkedList[T]) yieldPrev(curr *node[T]) *node[T]{
 	return curr.prev
 }
 
-func (self *doublyLinkedList[T]) insertAtMid(data T) {
-	newMiddleNode := &node[T]{ data: data }
-	if self.length == 0 && self.head == nil {
-		self.head = newMiddleNode
-		self.tail = newMiddleNode
-		self.length++
-		return
+func (self *doublyLinkedList[T]) insertAtMid(data T) (error) {
+	return self.insertAtNth(data, int(math.Ceil(float64(self.length)/2.0)))
+}
+
+func (self *doublyLinkedList[T]) insertAtNth(data T, position int) (error) {
+	if position > self.length+1 {
+		return errors.New("Could not insert into list, requested position larger than post-insertion list size")
 	}
-	if self.length == 1 {
-		self.tail = self.head
-		self.head = newMiddleNode
-		self.length++
-		return
+	if position <= 0 {
+		return errors.New("Could not insert into list, requested position should be larger or equal than 1")
 	}
-	oldMidNode := self.yieldNth(self.yieldNext,self.head, int(math.Ceil(float64(self.length)/2.0)))
+	newNthNode := &node[T]{ data: data }
+	if position == 1 {
+		self.frontPush(data)
+		return nil
+	}
+	if position == self.length+1 {
+		self.backPush(data)
+		return nil
+	}
+	oldMidNode, _ := self.yieldNth(self.yieldNext,self.head,position)
 	nodeNthPlus1 := oldMidNode.next
-	newMiddleNode.prev = oldMidNode
-	newMiddleNode.next = nodeNthPlus1
-	oldMidNode.next = newMiddleNode
-	nodeNthPlus1.prev = newMiddleNode
+	newNthNode.prev = oldMidNode
+	newNthNode.next = nodeNthPlus1
+	oldMidNode.next = newNthNode
+	nodeNthPlus1.prev = newNthNode
 	self.length++
+	return nil
 }
 
 func main() {
     list := createIntDoublyLinkedList()
 	list.frontPush(1)
-	list.backPush(2)
+	list.insertAtNth(2,2)
     list.backPush(4)
-    list.backPush(5)
+    list.insertAtNth(5,4)
 	fmt.Printf("Initial list:\n")
 	list.traverse(list.yieldNext, list.head)
 	list.insertAtMid(3)
@@ -131,14 +138,14 @@ func main() {
 	list.traverse(list.yieldNext, list.head)
 
     list2 := createIntDoublyLinkedList()
-	list2.frontPush(5)
-	list2.backPush(10)
+	list2.insertAtNth(10,1)
     list2.backPush(4)
     list2.backPush(32)
     list2.backPush(16)
-	fmt.Printf("Initial list:\n")
+	list2.frontPush(5)
+	fmt.Printf("Initial list2:\n")
 	list2.traverse(list2.yieldNext, list2.head)
 	list2.insertAtMid(41)
-	fmt.Printf("Final list insert 41 at mid:\n")
+	fmt.Printf("Final list2 insert 41 at mid:\n")
 	list2.traverse(list2.yieldNext, list2.head)
 }
